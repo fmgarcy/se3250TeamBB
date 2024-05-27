a63a86338d360be6c0a5cda18890aeda310d58e7
fmgarcy <feonagarcia@gmail.com>
2024-05-26 22:20:06 -0700

    Update __init__.py

--- se3250lib/__init__.py
+++ se3250lib/__init__.py
@@ -1,3 +1,3 @@
 from .cost_eq import calculate_ship_cost, calculate_weap_cost
 from .ships_inv import Ship, get_ship_data, list_all_ships
-from .wpninv import Missiles, get_wpn_data, list_all_weapons, get_offensive_mod
+from .wpn_inv import Missiles, get_wpn_data, list_all_weapons, get_offensive_mod
