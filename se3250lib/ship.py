acf4b9d5910836c7922e548f37417df0a8a75237
fmgarcy <feonagarcia@gmail.com>
2024-06-09 03:23:38 -0700

    Update ship.py

--- se3250lib/ship.py
+++ se3250lib/ship.py
@@ -17,62 +17,80 @@ ship_inv = {
         "classtype": 'LCS',
         "op": 24,
         "dp": 21,
-        "sp": 1.15
+        "sp": 1.15,
+        "ship_age": 8,
+        "crew_training": 0.85
     },
     "LCS-Independence": {
         "type": 'LCS-Independence',    
         "classtype": 'LCS',
         "op": 32,
         "dp": 11,
-        "sp": 1.27
+        "sp": 1.27,
+        "ship_age": 6,
+        "crew_training": 0.88
     },
     "DDG-Arleigh-Burke": {
         "type": 'DDG-Arleigh-Burke',    
         "classtype": 'DDG',
         "op": 104,
         "dp": 90,
-        "sp": 1.54
+        "sp": 1.54,
+        "ship_age": 12,
+        "crew_training": 0.92
     },
     "CG-Ticonderoga": {
         "type": 'CG-Ticonderoga',    
         "classtype": 'CG',
         "op": 138,
         "dp": 68,
-        "sp": 1.73
+        "sp": 1.73,
+        "ship_age": 18,
+        "crew_training": 0.88
     },
     "Destroyer-055D-Renhai": {
         "type": 'Destroyer-055D-Renhai',
         "classtype": 'Destroyer',
         "op": 112,
         "dp": 24,
-        "sp": 1.8
+        "sp": 1.8,
+        "ship_age": 4,
+        "crew_training": 0.92
     },
     "Destroyer-052D-Luyang-III": {
         "type": 'Destroyer-052D-Luyang-III',
         "classtype": 'Destroyer',
         "op": 64,
         "dp": 24,
-        "sp": 1.57
+        "sp": 1.57,
+        "ship_age": 8,
+        "crew_training": 0.9
     },
     "Frigate-054A-Jiangkai-II": {
         "type": 'Frigate-054A-Jiangkai-II',
         "classtype": 'Frigate',
         "op": 8,
         "dp": 32,
-        "sp": 1.34
+        "sp": 1.34,
+        "ship_age": 10,
+        "crew_training": 0.87
     },
     "Frigate-053H3-Jiangwei-II": {
         "type": 'Frigate-053H3-Jiangwei-II',
         "classtype": 'Frigate',
         "op": 8,
         "dp": 8,
-        "sp": 1.12
+        "sp": 1.12,
+        "ship_age": 14,
+        "crew_training": 0.84
     },
     "Corvette-056A-Jiangdao": {
         "type": 'Corvette-056A-Jiangdao',
         "classtype": 'Corvette',  
         "op": 4,
         "dp": 8,
-        "sp": 0.9
+        "sp": 0.9,
+        "ship_age": 6,
+        "crew_training": 0.88
     }
 }
